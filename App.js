b7930cfd19f323e3ebee976c76d8d5bc607f661c
Nimish Kashyap <nimish.kashyap2020@vitstudent.ac.in>
2022-10-01 12:41:09 +0530

    Revert "Added example stack navigator"
    This reverts commit 2c783773e2e51bfa38bbe156be830258de6cb4ab.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {} from "@react-navigation/native-stack";
 import {
   StyleSheet,
   Text,
@@ -11,8 +11,6 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -33,9 +31,27 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <View style={styles.container}>
+        <Image style={styles.image} source={require("./assets/logo.jpg")} />
+
+        <StatusBar style="auto" />
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Email."
+            placeholderTextColor="#003f5c"
+            onChangeText={(phone) => setPhoneNumber(phone)}
+          />
+        </View>
+
+        <TouchableOpacity>
+          <Text style={styles.forgot_button}>Forgot Password?</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
     </NavigationContainer>
   );
 }
